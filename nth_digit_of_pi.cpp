49e64b137302535f1f9be4941880071530fec527
bmorton12 <morton1654@gmail.com>
2019-11-30 11:12:03 -0500

    Calculate the nth digit of pi
    Calculate the nth digit of pi

--- nth_digit_of_pi.cpp
+++ nth_digit_of_pi.cpp
@@ -1,11 +1,16 @@
+/*
+https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
+https://www.davidhbailey.com/dhbpapers/bbp-alg.pdf
+*/
+
 #include<iostream>
 #include<iomanip>
 #include<cmath>
 
-// x^(y) % p
+// calculate x^(y) % p, https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
 double modexp(double x, double y, double p)
 {
-	double res = 1;
+	double remainder = 1;
 
 	//Update x if it is greater than or equal to p
 	x = std::fmod(x, p);
@@ -14,53 +19,13 @@ double modexp(double x, double y, double p)
 	{
 		// If y is odd, multiply x with result
 		if (((int)y & 1) == 1)
-			res = std::fmod(res * x, p);
+			remainder = std::fmod(remainder * x, p);
 		// y must be even now
 		y = (int)y >> 1;
 		x = std::fmod(x * x, p);
 	}
-	return res;
+	return remainder;
 }
-
-double expm(double p, double ak)
-{
-	int i, j;
-	double p1, pt, r;
-#define ntp 25
-	static double tp[ntp];
-	static int tp1 = 0;
-
-	if (tp1 == 0) {
-		tp1 = 1;
-		tp[0] = 1.;
-
-		for (i = 1; i < ntp; i++) tp[i] = 2. * tp[i - 1];
-	}
-
-	if (ak == 1.) return 0.;
-
-	for (i = 0; i < ntp; i++) if (tp[i] > p) break;
-
-	pt = tp[i - 1];
-	p1 = p;
-	r = 1.;
-
-	for (j = 1; j <= i; j++) {
-		if (p1 >= pt) {
-			r = 16. * r;
-			r = r - (int)(r / ak) * ak;
-			p1 = p1 - pt;
-		}
-		pt = 0.5 * pt;
-		if (pt >= 1.) {
-			r = r * r;
-			r = r - (int)(r / ak) * ak;
-		}
-	}
-
-	return r;
-}
-
 
 double getS_j(int n, int j)
 {
@@ -89,18 +54,16 @@ double getS_j(int n, int j)
 
 double getnthpi(long n)
 {
-	double pid =  4 * getS_j(n, 1) - 2 * getS_j(n, 4) - getS_j(n, 5) - getS_j(n, 6);
-	double fractionalpart = std::modf(pid, &fractionalpart);
-	pid = pid - (int)pid + 1.0;
-	return pid;
+	double fractionalpart = std::modf(4 * getS_j(n, 1) - 2 * getS_j(n, 4) - getS_j(n, 5) - getS_j(n, 6), &fractionalpart);
+	fractionalpart += 1.0;
+	return fractionalpart;
 }
 
 int main()
 {
-	//calculate the 0th digit of pi
-	std::cout << std::setprecision(15) << getnthpi(0) << std::endl;
-	//calculate the 1000000th digit of pi
-	std::cout << std::setprecision(15) << getnthpi(1000000) << std::endl;
+	/* last digit displayed is not accurate */
+	std::cout << std::setprecision(13) << getnthpi(0) << std::endl; //calculate the 0th digit of pi
+	std::cout << std::setprecision(13) << getnthpi(1000000) << std::endl; //calculate the 1000000th digit of pi
 	return 0;
 }
 
